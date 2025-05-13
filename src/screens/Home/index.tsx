0a8949b8f820c14cccdeb89d76ef26b48465ce48
lucaianl <lucaianml@gmail.com>
2025-05-13 09:38:43 -0300

    settings

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from "react-native";
+
+import { styles } from './styles';
+
+export default function Home(){ 
+    return(
+        <>
+        </>
+    )
+}
